89183f720030ffac2e3d3d3ffabc94679b8da008
Thomas <toma_judo_71@hotmail.fr>
2019-03-14 15:22:19 +0100

    v1.1

---
+++ src/test/java/EmployeTest.java
@@ -0,0 +1,21 @@
+import com.ipiecoles.java.java350.model.Employe;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+public class EmployeTest {
+    @Test
+    public void testGetNombreAnneeAncienneteNow(){
+        //Given
+        LocalDate dateEmbauche = LocalDate.now();
+        Employe employe = new Employe();
+        employe.setDateEmbauche(dateEmbauche);
+
+        //When
+        Integer nbAnnee = employe.getNombreAnneeAnciennete();
+
+        //Then
+        Assertions.assertThat(nbAnnee).isEqualTo(0);
+    }
+}
