437d0c6141d604d45b1773c0d33a9973bc99f964
Thomas <toma_judo_71@hotmail.fr>
2019-03-14 15:35:44 +0100

    v1.1

--- src/test/java/EmployeTest.java
+++ src/test/java/EmployeTest.java
@@ -18,4 +18,43 @@ public class EmployeTest {
         //Then
         Assertions.assertThat(nbAnnee).isEqualTo(0);
     }
+
+    @Test
+    public void testGetNombreAnneeAncienneteNull(){
+        //Given
+        Employe e = new Employe();
+        e.setDateEmbauche(null);
+
+        //When
+        Integer nbAnnee = e.getNombreAnneeAnciennete();
+
+        //Then
+        Assertions.assertThat(nbAnnee).isEqualTo(0);
+    }
+
+    @Test
+    public void testGetNombreAnneeAncienneteMoins2(){
+        //Given
+        Employe e = new Employe();
+        e.setDateEmbauche(LocalDate.now().minusYears(2));
+
+        //When
+        Integer nbAnnee = e.getNombreAnneeAnciennete();
+
+        //Then
+        Assertions.assertThat(nbAnnee).isEqualTo(2);
+    }
+
+    @Test
+    public void testGetNombreAnneeAnciennetePlus2(){
+        //Given
+        Employe e = new Employe();
+        e.setDateEmbauche(LocalDate.now().plusYears(2));
+
+        //When
+        Integer nbAnnee = e.getNombreAnneeAnciennete();
+
+        //Then
+        Assertions.assertThat(nbAnnee).isEqualTo(0);
+    }
 }
